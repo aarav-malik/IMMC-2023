b62ffaff0c9c9303b5bb38991274a2cf3a9980dd
MOOMINNN123 <aamin.mdzz@gmail.com>
2023-03-16 00:33:46 +0800

    hi hi another set of code

--- NPV.py
+++ NPV.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 npvs = pd.read_csv('data/NPV.csv', delimiter =',')
-expected_annual_return_rate = (npvs['profits']/npvs['initial invested value'])
+expected_annual_return_rate = (npvs['profits']/(npvs['revenue']-npvs['profits']))
 npvs['expected annual return rate'] = expected_annual_return_rate
 
 for npv in npvs.iterrows():
@@ -11,10 +11,14 @@ for npv in npvs.iterrows():
     present_value_for_year_3 = npvs['future present value for year 3']/(1 + npvs['expected annual return rate'])**3
     present_value_for_year_4 = npvs['future present value for year 4']/(1 + npvs['expected annual return rate'])**4
     present_value_for_year_5 = npvs['future present value for year 5']/(1 + npvs['expected annual return rate'])**5
+    
     total_present_value = present_value_for_year_1 + present_value_for_year_2 + present_value_for_year_3 + present_value_for_year_4 + present_value_for_year_5
 
 net_present_value = total_present_value - npvs['initial invested value']
-
+npvs['Total present value'] = total_present_value
 npvs['net present value'] = net_present_value
 
+for x in npvs.iterrows():
+    normalised_net_present_value = 1 + (((npvs['net present value']-np.nanmin(npvs['net present value']))*(10-1))/(np.nanmax(npvs['net present value'])-np.nanmin(npvs['net present value'])))
+    npvs['Normalised Score'] = normalised_net_present_value
 print(npvs)
