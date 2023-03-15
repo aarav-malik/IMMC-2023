149df54392412e7dbb990aa6d19dcaffdd72d8f9
MOOMINNN123 <aamin.mdzz@gmail.com>
2023-03-15 16:10:23 +0800

    Hello new stuff right here

--- Social benefits.py
+++ Social benefits.py
@@ -4,13 +4,20 @@ import numpy as np
 benefits = pd.read_csv('data/Socialbenefitsdata.csv', delimiter =',')
 salarys = pd.read_csv('data/Salary benefits data.csv', delimiter =',')
 
+
+
 for benefit in benefits.iterrows():
     social_interaction_benefits = (benefits['People affected'] * benefits['Magnitude'] * benefits['Duration'])/365
 
 benefits['Social interaction benefit score'] = social_interaction_benefits
 
-#normalising the data
+#normalised salary to create a magnitude of salary ranging between 1 to 4 inclusive.
+salary_magnitude = pd.read_csv('data/Normalised salary magnitude.csv', delimiter =',')
+for value in salary_magnitude.iterrows():
+    magnitude_salary = 1 + (((salary_magnitude['Salary']-np.nanmin(salary_magnitude['Salary']))*(4-1))/(np.nanmax(salary_magnitude['Salary'])-np.nanmin(salary_magnitude['Salary'])))
 
+salarys['Magnitude'] = magnitude_salary
+print(salarys)
 
 for salary in salarys.iterrows():
     salary_benefits = (salarys['People affected'] * salarys['Magnitude'] * salarys['Duration'])/365
